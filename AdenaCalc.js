92a710b1ed9cb2b6344fccbb6d6907ef63845806
123cmp <oxysoft123@gmail.com>
2015-12-22 13:25:45 +0200

    Add calculator

---
+++ AdenaCalc.js
@@ -0,0 +1,97 @@
+function AdenaCalc(moneyField, adenaField) {
+
+    var self = this;
+
+    const MONEY_TO_ADENA = 1,
+          ADENA_TO_MONEY = 0;
+
+    self.coefficients = [{
+        value: 7,
+        count: 1000
+    }, {
+        value: 8,
+        count: 2000
+    }, {
+        value: 10,
+        count: 5000
+    }];
+
+    self.chosenCoefficient = self.coefficients[0];
+
+    moneyField.keyup(function() {
+        var value = moneyField.val();
+        var coefficientId = findCoefficient(value, MONEY_TO_ADENA);
+        chooseCoefficient(coefficientId);
+        calculate(MONEY_TO_ADENA);
+    });
+
+    adenaField.keyup(function() {
+        var value = adenaField.val();
+        var coefficientId = findCoefficient(value, ADENA_TO_MONEY);
+        chooseCoefficient(coefficientId);
+        calculate(ADENA_TO_MONEY);
+    });
+
+    function findCoefficient(value, direction) {
+        var coefficientId = 0;
+        var convertFunction = function(value) { return value };
+        if(direction == MONEY_TO_ADENA)
+            convertFunction = moneyToAdena;
+
+        var coefficientsCount = self.coefficients.length,
+            lastCoefficientId = coefficientsCount - 1,
+            lastCoefficient = self.coefficients[lastCoefficientId],
+            firstCoefficient = self.coefficients[0];
+
+        if(convertFunction(value, firstCoefficient) < firstCoefficient.count)
+            coefficientId = 0;
+        else if(convertFunction(value, lastCoefficient) > lastCoefficient.count)
+            coefficientId = self.coefficients.length - 1;
+        else
+            self.coefficients.forEach(function(coefficient, i) {
+                if(convertFunction(value, coefficient) >= coefficient.count) {
+                    coefficientId = i
+                }
+            });
+
+        return coefficientId;
+    }
+
+    function setCoefficients(array) {
+        if(!array || !array.length) return;
+        self.coefficients = array;
+    }
+
+    function chooseCoefficient(id) {
+        self.chosenCoefficient = self.coefficients[id];
+    }
+
+    function moneyToAdena(money, coefficient) {
+        if(!coefficient) coefficient  = self.chosenCoefficient;
+        return money * coefficient.value;
+    }
+
+    function adenaToMoney(adena, coefficient) {
+        if(!coefficient) coefficient  = self.chosenCoefficient;
+        return adena / coefficient.value;
+    }
+
+    function calculate(direction) {
+        console.info(
+            "Direction: ", direction ? "money -> adena" : "adena -> money\n",
+            "Coefficient: ", self.chosenCoefficient
+        );
+
+        if(direction) { // money -> adena
+            adenaField.val(moneyToAdena(moneyField.val()));
+        } else {        // adena -> money
+            moneyField.val(adenaToMoney(adenaField.val()));
+        }
+    }
+
+    return {
+        setCoefficients: setCoefficients,
+        chooseCoefficient: chooseCoefficient,
+        calculate: calculate
+    }
+}
