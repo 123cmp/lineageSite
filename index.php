35307d6afe99df1cda6b66f5c0ce43462aa16f84
eIIIkuHKoT <chezara40@gmail.com>
2016-01-12 10:56:03 +0300

    new bg image amd online-chat

--- index.php
+++ index.php
@@ -166,5 +166,11 @@
     <script src="/bower_components/jquery-validation/dist/jquery.validate.min.js"></script>
     <script src="/bower_components/notific8/dist/jquery.notific8.min.js"></script>
     <script src="/scripts/AdenaCalc.js"></script>
+
+    <!-- BEGIN JIVOSITE CODE {literal} -->
+	<script type='text/javascript'>
+	(function(){ var widget_id = 'RSSV6viEt9';
+	var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();</script>
+	<!-- {/literal} END JIVOSITE CODE -->
 </body>
 </html>
